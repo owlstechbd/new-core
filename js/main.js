4481d62af646d9ff43279d7e65dc19e3762d26dc
owlstechbd <owlstech.net@gmail.com>
2024-11-05 20:03:49 +0600

    changes a lost

--- js/main.js
+++ js/main.js
@@ -100,7 +100,44 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+//slider-hero
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
 
+// Function to change slide
+function changeSlide(direction) {
+    slides[currentSlide].classList.remove('active');
+
+    currentSlide += direction;
+
+    // Looping around the slides
+    if (currentSlide < 0) {
+        currentSlide = totalSlides - 1; // Loop to last slide
+    } else if (currentSlide >= totalSlides) {
+        currentSlide = 0; // Loop to first slide
+    }
+
+    slides[currentSlide].classList.add('active');
+    document.querySelector('.slider').style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Auto slide function
+function autoSlide() {
+    changeSlide(1);
+}
+
+// Set interval for auto sliding every 3 seconds
+let autoSlideInterval = setInterval(autoSlide, 5000);
+
+// Clear the interval on mouse enter and set it again on mouse leave
+document.querySelector('.slider-container').addEventListener('mouseenter', () => {
+    clearInterval(autoSlideInterval);
+});
+
+document.querySelector('.slider-container').addEventListener('mouseleave', () => {
+    autoSlideInterval = setInterval(autoSlide, 6000);
+});
 
 
 
